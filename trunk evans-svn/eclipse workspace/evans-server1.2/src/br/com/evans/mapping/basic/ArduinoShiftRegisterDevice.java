bdbdd7f57f067e2b56862d1e194cff5e70155d64
gcats <gcats@6ba9e0cf-9736-4421-8acc-2477f75daab5>
2013-03-10 21:38:13 +0000

    Added Device implementation concepts

---
+++ trunk evans-svn/eclipse workspace/evans-server1.2/src/br/com/evans/mapping/basic/ArduinoShiftRegisterDevice.java
@@ -0,0 +1,23 @@
+package br.com.evans.mapping.basic;
+
+public class ArduinoShiftRegisterDevice implements DeviceImpl{
+	
+	@Override
+	public boolean getDeviceStatus() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void switchStates() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getInterfaceState() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- trunk evans-svn/eclipse workspace/evans-server1.2/src/br/com/evans/mapping/basic/Device.java
+++ trunk evans-svn/eclipse workspace/evans-server1.2/src/br/com/evans/mapping/basic/Device.java
@@ -9,21 +9,51 @@ package br.com.evans.mapping.basic;
  * should stablish with serial/rf/wifi, and what kind of
  * information it should output to the device)
  * 
+ * ---------------------------- First Example --------------------------------
  * 1) Each device will have its own switching state method implementation,
  * for instance, it is a light connected to a arduino through a shift 
  * registered relay on pin 3. 
  * 2) The implementation method should handle the arduino connection 
  * and send a string that contains a shift register binary converted 0-255
  * 3) The arduino should receive correctly the string, change the states 
- * and send a string back, reporting the states 
+ * and send a string back, reporting the states (with a decimal number) to 
+ * the mainframe (this server), device order has to be strictly defined so that
+ * the mainframe doesn't mix up which device it should turn on/off
+ * 3.5) A function similar to toArduinoShiftRegisterCode from DeviceMonitor must 
+ * be made on the arduino, so manual overrides work
+ * 4) This way manual overrides also get reported to the mainframe (this server)
+ * 
+ * ---------------------------- Second Example --------------------------------
+ * 1) For instance, it is a light connected to a arduino through a relay 
+ * on pin 4 of the arduino 
+ * 2) The implementation method should handle the arduino connection 
+ * and send (via serial) a string that contains the string linked to the 
+ * device with the right format ("o_")
+ * 3) The arduino should receive correctly the string, change the states 
+ * and send a string back, reporting the states to the mainframe (this server)
+ * It can send two kinds of information. Maybe a binary -> decimal converted
+ * number (assuming you know the order of the device in the pin	)
+ * 4) This way manual overrides also get reported to the mainframe (this server)
+ * 
+ * ---------------------------- Third Example --------------------------------
+ * 1) For instance, it is a light connected to a arduino through x10 
+ * appliance module
+ * 2) The implementation method should handle the x10 connection 
+ * and send (via serial) the state changing string to the x10 transmitter
+ * 3) The x10 does all the work, It only doesn't report states.
+ * 
+ * ---------------------------- Forth Example --------------------------------
+ * 1) For instance, it is a input connected to a EletricImp 
+ * 2) The implementation method should handle a POST from the EletricImp,
+ * understanding what kind of info will be transmitted, so data can be 
+ * processed correctly
  * @author Gcats
  *
  */
-public class Device {
+public class Device implements DeviceImpl {
 	private int id;
 	private String rf_code;
 	private boolean status;
-	private boolean isOutput;
 	
 	/**
 	 * Constructor of Device class
@@ -34,11 +64,9 @@ public class Device {
 	 * @param status status of the device: whether is on/off
 	 * @param isOutput 
 	 */
-	public Device(int id, String rf_code, boolean status, boolean isOutput) {
+	public Device(int id, boolean status) {
 		this.id = id;
-		this.rf_code = rf_code;
 		this.status = status;
-		this.isOutput = isOutput;
 	}
 	
 	public int getId() {
@@ -48,29 +76,22 @@ public class Device {
 	public void setId(int id) {
 		this.id = id;
 	}
-	
-	public String getRf_code() {
-		return rf_code;
-	}
-	
-	public void setRf_code(String rf_code) {
-		this.rf_code = rf_code;
-	}
-	
-	public boolean isStatus() {
+
+	@Override
+	public boolean getDeviceStatus() {
 		return status;
 	}
 
-	public void setStatus(boolean status) {
-		this.status = status;
+	@Override
+	public void switchStates() {
+		// TODO Auto-generated method stub
+		
 	}
 
-	public boolean isOutput() {
-		return isOutput;
-	}
-
-	public void setOutput(boolean isOutput) {
-		this.isOutput = isOutput;
+	@Override
+	public int getInterfaceState() {
+		// TODO Auto-generated method stub
+		return 0;
 	}
 	
 }
