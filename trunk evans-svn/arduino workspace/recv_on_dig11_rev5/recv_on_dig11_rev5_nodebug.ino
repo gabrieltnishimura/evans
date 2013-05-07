78d49e54957396dca5dc5691e79400dcbe86a503
gcats <gcats@6ba9e0cf-9736-4421-8acc-2477f75daab5>
2013-05-07 21:47:45 +0000

    Modified message length and added half duplex support (2.0)

---
+++ trunk evans-svn/arduino workspace/recv_on_dig11_rev5/recv_on_dig11_rev5_nodebug.ino
@@ -0,0 +1,126 @@
+#include <VirtualWire.h>
+#define BUTTON_PIN 2
+#define RX_MODULE_PIN 11
+#define TX_MODULE_PIN 13
+#define NDEV 4
+
+/** R5 uses somewhat a new concept. 
+ * As the mainframe is now sending messages 
+ * telling the appliance to switch whether 
+ * on or off, implementation of a new 
+ * message identifier had to be done. 
+ * In R5, major changes to wasMessageReceived 
+ * and the loop structure was modified a bit as
+ * well, aand, added message feedback. */
+
+
+// button stuff
+int readingButton;           // the current reading from the input pin
+int previousButton = LOW;    // the previous reading from the input pin
+
+// random stuff
+long time = 0;         // the last time the output pin was toggled
+long debounce = 175;   // the debounce time, increase if the output flickers
+// state
+char *id_off[NDEV] = {"a", "c", "e", "g"};
+char *feedback[NDEV*2] = {"B", "b", "D", "d", "F", "f", "H", "h"};
+char *id_on[NDEV] = {"A", "B", "C", "D"};
+int pins[NDEV] = {2, 3, 4, 5};
+
+boolean on[NDEV] = {false, false, false, false};
+int switchPin = 0;
+int led_iterator;
+
+void setup()
+{
+    pinMode(BUTTON_PIN, INPUT);
+    
+    // multiple outputs
+    for(led_iterator = 0; led_iterator < NDEV; led_iterator++) {
+      pinMode(pins[led_iterator], OUTPUT);
+    }   
+    
+    vw_setup(2000);	 // Bits per sec
+    vw_set_rx_pin(RX_MODULE_PIN);
+    vw_set_tx_pin(TX_MODULE_PIN);
+    vw_rx_start();
+}
+
+void loop()
+{
+	if (wasMessageReceived()) {
+		switchRelayState();
+	}
+}
+
+void switchRelayState()
+{
+    if (on[switchPin]) {
+      digitalWrite(pins[switchPin], LOW);
+      on[switchPin] = false;
+    } else {
+      digitalWrite(pins[switchPin], HIGH);
+      on[switchPin] = true;
+    }
+	sendStateMessage(on[switchPin]); // since it's changed states already, why not report to the mainframe
+}
+
+boolean wasMessageReceived()
+{
+	uint8_t buf[VW_MAX_MESSAGE_LEN] = "";
+	uint8_t buflen = VW_MAX_MESSAGE_LEN;
+	boolean rightString = false;
+  
+	if (vw_get_message(buf, &buflen)) {
+		for(led_iterator = 0; led_iterator < NDEV; led_iterator++) {
+			switchPin = led_iterator;
+			if (strlen((char*)buf) != 1) { // received message length must be 1
+			 rightString = false;
+			} else {
+				rightString = true;
+				if (on[led_iterator]) { // if is on, see if it commands to switch off
+					if ((uint8_t)id_off[led_iterator][0] != buf[0]) {
+						rightString = false;
+					} // if equals
+				} else { // if is off, see if it commands to switch on
+					if ((uint8_t)id_on[led_iterator][0] != buf[0]) {
+						rightString = false;
+					} // if equals
+				}
+				if (rightString) {
+					break;
+				}
+			} // if length is equal
+			 
+			if (rightString) {
+				break;
+			}
+		}// iterates through all leds
+	}
+	return rightString;
+}
+
+boolean wasButtonPressed()
+{
+	readingButton = digitalRead(BUTTON_PIN);
+	boolean pressed = false;
+  
+	if (readingButton == HIGH && previousButton == LOW && millis() - time > debounce) {
+		time = millis();
+		pressed = true;
+	}
+  
+	previousButton = readingButton;
+	return pressed;
+}
+
+void sendStateMessage(boolean state)
+{
+	if (state) {
+		strcpy(msg, feedback[switchPin]); // sends the next char
+	} else {
+		strcpy(msg, feedback[switchPin + 1]);
+	}
+	vw_send((uint8_t *)msg, strlen(msg));
+	vw_wait_tx();
+}
