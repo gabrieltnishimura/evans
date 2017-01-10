cd60b82f3203148ce94609cea95f0516bf841cb6
Nishimura <gabriel.t.nishimura@accenture.com>
2017-01-10 11:05:32 -0200

    Removing files

--- front/app/modules/ts/app.module.ts
+++ front/app/modules/ts/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './main/app.component';
+import { EvansSidebar }  from './components/evans-sidebar/evans-sidebar.component';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, EvansSidebar ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ front/app/modules/ts/components/evans-sidebar/evans-sidebar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'evans-sidebar',
+  templateUrl: 'app/modules/ts/components/evans-sidebar/evans-sidebar.component.html',
+})
+export class EvansSidebar  {
+    constructor() {
+        console.log("heyo from evans sidebar");
+    }
+}
